065d3431e0184fe9a6d2bf831e720df33714353c
galiano <edigaliano@gmail.com>
2023-02-17 12:08:52 -0300

    Forma mais Correta de Contrução do UseState

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -2,22 +2,39 @@ import React, { useState } from "react";
 
 function App() {
 
-    const [valor, setValor] = useState(0);
+    // const [valor, setValor] = useState(100);
 
-    function incremento() {
-        setValor(valor + 1)
-    }
+    // function incremento() {
+    //     setValor(valor_antigo => valor_antigo + 1)
+    // }
 
-    function decremento() {
-        setValor(valor - 1)
+    // function decremento() {
+    //     setValor(valor_antigo => valor_antigo - 1)
+    // }
+
+    const [playerName, setPlayerName] = useState(() => {
+        return "João"
+    })
+
+    const [score, setScore] = useState(() => {
+        return 0
+    })
+
+    function aumentar() {
+        setScore(sc_antigo => {
+            return sc_antigo + 1
+        })
     }
     
     return (
         <>
-            <h1>Hello World</h1>
-            <button onClick={decremento}>Decrementar</button>
-            {valor}
-            <button onClick={incremento}>Incrementar</button>
+            <h1>React Hooks - useStates</h1>
+            <h2>Nome: {playerName}</h2>
+            <h2>Score: {score}</h2>
+            <button onClick={aumentar}>Aumentar</button>
+            {/* <button onClick={decremento}>Decrementar</button>
+            <span>{valor}</span>
+            <button onClick={incremento}>Incrementar</button> */}
             
             
         </>
