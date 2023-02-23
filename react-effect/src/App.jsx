82adbdc9c35cc77d2f1852f02b1c60972f87a711
galiano <edigaliano@gmail.com>
2023-02-23 19:51:44 -0300

    Consumindo Dados Com Fetch e  useEffect

--- react-effect/src/App.jsx
+++ react-effect/src/App.jsx
@@ -1,45 +1,123 @@
 import React, { useEffect, useState } from "react";
+import Users from "./components/Users";
+import './App.css'
 
-export default function App() {
+function App() {
 
-    const [info, setInfo] = useState('clientes')
-    const [valor, setValor] = useState(100)
-    
-    const clientes = ["João", "Ana", "Carlos"]
-    const produtos = ["Pão", "Manteiga", "Leite"]
-    const vendas = ["1 x Pão (João)", "2 x Manteiga (Ana)", "3 x (Carlos)"]
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
-        switch (info) {
-            case 'clientes':
-                console.table(clientes)
-                break;
-            case 'produtos':
-                console.table(produtos)
-                break;
-            case 'vendas':
-                console.table(vendas)
-                break;
-                
-        }
-    }, [info])
+        fetch('https://dummyjson.com/users')
+            .then(res => res.json())
+            .then(json => {
+                setUsers(json.users)
+            })
+    }, [])
 
-    useEffect(() => {
-        console.log('TESTE')
-    }, [valor])
-    
     return (
         <>
-            <h1>React Hooks - use Effect</h1>
-            <hr />
-            <button onClick={() => setInfo('clientes')}>Clientes</button>
-            <button onClick={() => setInfo('produtos')}>Produtos</button>
-            <button onClick={() => setInfo('vendas')}>Vendas</button>
-
-            <h3>{info}</h3>
-            <h3>{valor}</h3>
-            <button onClick={() => setValor(valor + 10)}>Vendas</button>
-            
+            <h3>Usúarios</h3>
+            {users.map(user => {
+                return <Users key={user.id} user={user}/>
+                    
+            })}
         </>
     )
 }
+
+
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function App() {
+
+//     const [info, setInfo] = useState('clientes')
+//     const [valor, setValor] = useState(100)
+    
+//     const clientes = ["João", "Ana", "Carlos"]
+//     const produtos = ["Pão", "Manteiga", "Leite"]
+//     const vendas = ["1 x Pão (João)", "2 x Manteiga (Ana)", "3 x (Carlos)"]
+
+//     useEffect(() => {
+//         switch (info) {
+//             case 'clientes':
+//                 console.table(clientes)
+//                 break;
+//             case 'produtos':
+//                 console.table(produtos)
+//                 break;
+//             case 'vendas':
+//                 console.table(vendas)
+//                 break;
+                
+//         }
+//     }, [info])
+
+//     useEffect(() => {
+//         console.log('TESTE')
+//     }, [valor])
+    
+//     return (
+//         <>
+//             <h1>React Hooks - use Effect</h1>
+//             <hr />
+//             <button onClick={() => setInfo('clientes')}>Clientes</button>
+//             <button onClick={() => setInfo('produtos')}>Produtos</button>
+//             <button onClick={() => setInfo('vendas')}>Vendas</button>
+
+//             <h3>{info}</h3>
+//             <h3>{valor}</h3>
+//             <button onClick={() => setValor(valor + 10)}>Vendas</button>
+            
+//         </>
+//     )
+// }
